06d4e2b75e273fe9acdd5b45fdef21ad0e347ecd
ohenrydevopr <ohenrydevopr@gmail.com>
2022-10-16 23:30:09 -0300

    Feat: Redirect added to gh profile

--- next.config.js
+++ next.config.js
@@ -3,6 +3,18 @@ const nextConfig = {
 	reactStrictMode: true,
 	swcMinify: true,
 	poweredByHeader: false,
-}
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: 'https://github.com/ohenrydevopr',
+				permanent: false,
+			},
+		];
+	},
+};
